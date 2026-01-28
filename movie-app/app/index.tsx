41beca5c9b1fc46e1c7ead6580f0ac32059a98db
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-01-28 17:06:27 +0500

    feat: create initial app entry point displaying a welcome message

--- movie-app/app/index.tsx
+++ movie-app/app/index.tsx
@@ -1,16 +1,9 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-primary">Welcome to Movie App!</Text>
-      <Link href="/onboarding" className="text-primary">
-        Go to Onboarding
-      </Link>
-      <Link href={{ pathname: "/movie/[id]", params: { id: "avengers" } }} className="text-primary">
-        Go to Movie 1
-      </Link>
     </View>
   );
 }
